38321311fbf38a1ae5afcf5de90758d284cafd33
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-12-09 17:25:01 +0700

    fix logic handle dayoff

--- src/app/controllers/DayOffController.js
+++ src/app/controllers/DayOffController.js
@@ -3,73 +3,67 @@ const Groups = require("../models/Groups");
 const Notifies = require("../models/Notifies");
 const User = require("../models/Users");
 class DayOffController {
-  createDayOff( req, res, next ) {
+  createDayOff(req, res, next) {
     User.findOne({
-      _id: req.body.userID ,
+      _id: req.body.userID,
     })
-      .populate('_id')
-      .then( user => {
+      .populate("_id")
+      .then((user) => {
         const userId = user.id;
-        const arrMaster = [];
         Groups.find({
-          memberID : userId,
+          memberID: userId,
         })
-          .then(groups => {
-            groups.forEach( group => {
-              arrMaster.push(...group.masterID)
-              const data = new DayOff({
-                userId: userId,
-                reason: req.body.reason,
-                fromDay: req.body.fromDay,
-                toDay: req.body.toDay,
-                quantity: req.body.quantity,
-                partialDay: req.body.partialDay,
-                listMaster : [
-                  {
-                    masterId:  group.masterID,
-                  }
-                ],
-              })
-              data
-                .save()
-                .then(() => {
-                  return res.josn(data)
-                      .status(200)
-                })
-                // .then(dayoff => {
-                //   const dataNotifies = new Notifies({
-                //     dayoffID: dayoff.id,
-                //     masterID: arrMaster,
-                //     status: dayoff.status,
-                //     note: '',
-                //   })
-                //   dataNotifies.save()
-                //   return res.json(data).status(200)  
-                // }) 
-                .catch(err => {
-                  return res.json(err)
-                    .status(400)
-                })   
-            })
-          })       
-          .catch(err => {
-            return res.json(err)
-              .status(400)
+          .then((groups) => {
+            const arrMasterId = [];
+            groups.forEach((group) => {
+              arrMasterId.push(group.masterID);
+            });
+            const newArrMaster = arrMasterId.toString().split(',');
+            const newArrMasterId = Array.from(new Set(newArrMaster))
+         
+            const data = new DayOff({
+              userId: userId,
+              reason: req.body.reason,
+              fromDay: req.body.fromDay,
+              toDay: req.body.toDay,
+              quantity: req.body.quantity,
+              partialDay: req.body.partialDay,
+              listMaster: newArrMasterId,
+            });
+            try {
+              data.save().then((data) => {
+                const dataNotifies = new Notifies({
+                  dayoffID: data.id,
+                  masterID: newArrMasterId,
+                  status: data.status,
+                  note: "",
+                });
+                
+                dataNotifies.save();
+
+                return res.json(data)
+                          .status(200)
+              });
+            } catch (error) {
+              console.log(error);
+            }
           })
+          .catch((err) => {
+            return res.json(err).status(400);
+          });
       })
-      .catch(next)
+      .catch(next);
   }
 
   async getDayOff(req, res, next) {
-    
     try {
       const dayoff = await DayOff.findOne({
         _id: req.params.id,
-        status: 'Pending'
-      })
-      return res.json(dayoff)  } 
-    catch (error) {
-      return res.status(400)
+        status: "Pending",
+      });
+      return res.json(dayoff);
+    } catch (error) {
+      return res.status(400);
     }
   }
 }
