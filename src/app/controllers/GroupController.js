3a990742ab5acc0cd1b58b0eb279c7a25a8b1f32
NgoSinh <ngosinh0@gmail.com>
2022-12-11 18:48:42 +0700

    get all group success

--- src/app/controllers/GroupController.js
+++ src/app/controllers/GroupController.js
@@ -1,26 +1,34 @@
 const Groups = require("../models/Groups");
 const Users = require("../models/Users");
 class GroupController {
-  createGroup( req, res, next ) {
-    
+  createGroup(req, res, next) {
     Users.findOne({
       _id: req.body.memberID,
     })
-      .populate('_id')
-      .then( user => {
+      .populate("_id")
+      .then((user) => {
         const memberID = user._doc;
         const data = new Groups({
           nameGroup: req.body.nameGroup,
           memberID: memberID,
           masterID: req.body.masterID,
         });
-        data
-          .save()
-          .then(() => res.json(data)
-            .status(200)
-          )
+        data.save().then(() => res.json(data).status(200));
       })
-      .catch(next)
+      .catch(next);
+  }
+  // GET: group/list
+  async showGroup(req, res, next) {
+    const users = await Groups.find({});
+    try {
+      res.send(users);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+    // const Group = await Groups.find({ nameGroup: "group one" }).populate(
+    //   "Users"
+    // );
+    // console.log(Group);
   }
 }
 
--- src/routes/group.route.js
+++ src/routes/group.route.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const route = express.Router();
 
-const groupController = require('../app/controllers/GroupController');
+const groupController = require("../app/controllers/GroupController");
 
-route.post('/create', groupController.createGroup)
+route.post("/create", groupController.createGroup);
+route.get("/list", groupController.showGroup);
 
 module.exports = route;
