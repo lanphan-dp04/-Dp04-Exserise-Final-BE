b2a41c8a3e7c8ef4df8627b0106fc687660ebc20
NgoSinh <ngosinh0@gmail.com>
2022-12-15 15:05:33 +0700

    done feature write a api for update group

--- src/routes/group.route.js
+++ src/routes/group.route.js
@@ -7,6 +7,7 @@ const groupController = require("../app/controllers/GroupController");
 route.post("/create", groupController.createGroup);
 route.get("/list", groupController.showGroup);
 route.post("/new", groupController.newGroup);
+route.put("/update/:id", groupController.updateGroup);
 route.delete("/delete/:id", groupController.deleteGroup);
 route.get("/list/:id", groupController.showOneGroup);
 
