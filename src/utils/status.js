9e2f8832ce0f65b87d2c0282f129eaa7ce1b4ed2
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-12-29 10:23:47 +0700

    update link slack

--- src/utils/status.js
+++ src/utils/status.js
@@ -4,13 +4,13 @@ const RquestSTT = {
   REJECT: 'Rejected',
   UPDATED: 'Updated',
   REVERTED: 'Reverted',
-  CANCLE: 'Cancled',
+  CANCLE: 'Canceled',
   REQUEST_CHANGE: 'Request Change',
   DAY_OFF: 'Day Off',
 }
 const MasterRquestSTT = {
   APPROVED: 'Approved',
-  CANCLE: 'Cancled',
+  CANCLE: 'Canceled',
   REJECTED: 'Rejected',
   REQUEST_CHANGE: 'Request Change'
 
